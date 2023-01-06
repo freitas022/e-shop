9230d31a0bd6586f4ccc51ea658108c0aab1d0f8
Matheus Freitas <84873099+freitas022@users.noreply.github.com>
2023-01-06 16:53:35 -0300

    Projeto inicial

---
+++ src/test/java/com/meuportifolio/curso/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meuportifolio.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

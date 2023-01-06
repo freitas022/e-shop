4e5246b27cc13b0aed91799f545d991ee508f925
Matheus Freitas <84873099+freitas022@users.noreply.github.com>
2023-01-06 18:44:30 -0300

    Service layer, component registration

---
+++ src/main/java/com/meuportifolio/curso/services/UserService.java
@@ -0,0 +1,26 @@
+package com.meuportifolio.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.meuportifolio.curso.entities.User;
+import com.meuportifolio.curso.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}

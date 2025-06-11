528c9ac00208e6243f2c001fe18965d83c069115
Matheus Freitas <mf022rj@gmail.com>
2025-06-11 00:14:48 -0300

    Remove unused stock quantity parameter from Product instantiation in TestConfig

--- eshop-backend/src/main/java/com/myapp/config/TestConfig.java
+++ eshop-backend/src/main/java/com/myapp/config/TestConfig.java
@@ -48,11 +48,11 @@ public class TestConfig implements CommandLineRunner {
 		Category cat2 = new Category("Books");
 		Category cat3 = new Category("Computers");
 		
-		Product p1 = new Product("The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "", 100);
-		Product p2 = new Product("Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "", 100);
-		Product p3 = new Product("Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "", 100);
-		Product p4 = new Product("PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "", 100);
-		Product p5 = new Product("Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "", 100);
+		Product p1 = new Product("The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product("Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product("Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product("PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product("Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 		
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		productRepository.saveAll(Arrays.asList(p1, p2 , p3, p4, p5));

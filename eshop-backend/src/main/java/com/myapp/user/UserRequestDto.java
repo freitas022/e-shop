18754f4e0fe1885ec5da1fa4583846fad9c1f085
Matheus Freitas <mf022rj@gmail.com>
2025-06-13 23:12:55 -0300

    Enhance UserRequestDto with validation constraints for email, password, and phone

--- eshop-backend/src/main/java/com/myapp/user/UserRequestDto.java
+++ eshop-backend/src/main/java/com/myapp/user/UserRequestDto.java
@@ -1,6 +1,25 @@
 package com.myapp.user;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
-public record UserRequestDto(@NotBlank String name, @NotBlank String email, @NotBlank String password, @NotBlank String phone) {
-}
+public record UserRequestDto(
+
+        @NotBlank
+        @Size(min = 3, max = 50, message = "Enter a name between 3 and 50 characters")
+        String name,
+
+        @NotBlank
+        @Email(message = "Enter a valid email address")
+        String email,
+
+        @NotBlank
+        @Size(min = 8, max = 64, message = "Enter a password between 8 and 64 characters")
+        String password,
+
+        @NotBlank
+        @Pattern(regexp = "^[1-9]{2}9\\d{8}$", message = "Invalid Brazilian cellphone format")
+        String phone
+) {}
